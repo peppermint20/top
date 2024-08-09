290e992fd7161704cff03e9a0e1a795167aa548a
Ben <voben21@gmail.com>
2024-08-08 19:24:24 -0700

    rps-ui

--- javascript/rock-paper-scissors/script.js
+++ javascript/rock-paper-scissors/script.js
@@ -20,29 +20,49 @@ function getHumanChoice() {
 let humanScore = 0;
 let computerScore = 0;
 
-function playRound(humanChoice, computerChoice) {
+function playRound(event) {
+  let humanChoice = event.target.textContent;
   humanChoice = humanChoice.toLowerCase();
+  let computerChoice = getComputerChoice();
   if (humanChoice === computerChoice) {
     console.log("Draw!");
+    divText.textContent = "Draw!";
   } else if (
     (humanChoice === "rock" && computerChoice === "scissors") ||
     (humanChoice === "scissors" && computerChoice === "paper") ||
     (humanChoice === "paper" && computerChoice === "rock")
   ) {
     console.log(`Win!, ${humanChoice} beats ${computerChoice}`);
+    divText.textContent = `Win! ${humanChoice} beats ${computerChoice}`;
     humanScore++;
   } else {
-    console.log(`Lose!, ${computerChoice} beats ${humanChoice}`);
+    console.log(`Lose! ${computerChoice} beats ${humanChoice}`);
+    divText.textContent = `Lose! ${humanChoice} beats ${computerChoice}`;
     computerScore++;
   }
 
+  divScore.textContent = `Player score: ${humanScore}\nComputer score: ${computerScore}`;
   console.log(`Player score: ${humanScore}\nComputer score: ${computerScore}`);
 }
+let container = document.querySelector("#container");
+let rockButton = document.createElement("button");
+let paperButton = document.createElement("button");
+let scissorsButton = document.createElement("button");
 
-function playGame() {
-  for (let i = 0; i < 5; ++i) {
-    playRound(getHumanChoice(), getComputerChoice());
-  }
-}
+rockButton.textContent = "Rock";
+rockButton.addEventListener("click", playRound);
+paperButton.textContent = "Paper";
+paperButton.addEventListener("click", playRound);
+scissorsButton.textContent = "Scissors";
+scissorsButton.addEventListener("click", playRound);
 
-playGame();
+let divText = document.createElement("div");
+divText.textContent = "Choose your option!";
+let divScore = document.createElement("div");
+divScore.textContent = `Player score: ${humanScore}\nComputer score: ${computerScore}`;
+
+container.appendChild(rockButton);
+container.appendChild(paperButton);
+container.appendChild(scissorsButton);
+container.appendChild(divText);
+container.appendChild(divScore);
